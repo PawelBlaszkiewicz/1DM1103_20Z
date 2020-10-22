3af826e9e3d9e3589d76b588c818c70720f1da16
PawelBlaszkiewicz <pawel@DESKTOP-3GC9N9C.localdomain>
2020-10-22 15:08:32 +0200

    działa? działa

---
+++ sem3/p1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    
+    // float a1;
+    // float a2;
+     float a1 = (2);
+     float a2 =(3);
+    float v = a1 + a2;
+    printf("JEST Suma: %f \n",  v);
+}
